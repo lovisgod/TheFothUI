8e88fcb9f216ef024c87730715d5ff4e952b4ac4
lovisgod <olifedayo94@gmail.com>
2019-06-11 20:19:28 +0100

    Starting The forth Template

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const openmenu =()=>{
+    document.getElementById("side-menu").style.display="block";
+    document.getElementById("menu-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+
+};
+
+const closemenu =()=>{
+    document.getElementById("side-menu").style.display="none";
+    document.getElementById("menu-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+
+};
