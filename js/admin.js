4886fa84e9b7c80700381a202035762da5856228
lovisgod <olifedayo94@gmail.com>
2019-06-14 11:13:38 +0100

    FT- handle clicks

--- js/admin.js
+++ js/admin.js
@@ -1,47 +1,113 @@
-document.getElementById('service').style.display = 'none';
-document.getElementById('footer-admin').style.display = 'none';
-document.getElementById('admin-categories').style.display = 'none';
+document.getElementById("service").style.display = "none";
+document.getElementById("footer-admin").style.display = "none";
+document.getElementById("side-menu").style.display = "none";
+document.getElementById("admin-categories").style.display = "none";
 var Flist = document.getElementsByTagName("li");
 
-const openmenu =()=>{
-    document.getElementById("side-menu").style.display="block";
-    document.getElementById("menu-btn").style.display="none";
-    document.getElementById("close-btn").style.display="block";
-
+const openmenu = () => {
+  document.getElementById("side-menu").style.display = "block";
+  document.getElementById("menu-btn").style.display = "none";
+  document.getElementById("close-btn").style.display = "block";
 };
 
-const closemenu =()=>{
-    document.getElementById("side-menu").style.display="none";
-    document.getElementById("menu-btn").style.display="block";
-    document.getElementById("close-btn").style.display="none";
-
+const closemenu = () => {
+  document.getElementById("side-menu").style.display = "none";
+  document.getElementById("menu-btn").style.display = "block";
+  document.getElementById("close-btn").style.display = "none";
 };
 
-
-const getUrl =(e)=> {
-    
-    const stationName = e.target.textContent;
-    console.log(stationName);
+const getUrl = e => {
+  var stationName = e.target.textContent;
+  switch (stationName) {
+    case "Add Service":
+      document.querySelector(".login-area").style.display = "none";
+      // this checks if browser is mobile
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        // some code..
+        document.getElementById("side-menu").style.display = "none";
+      } else {
+        document.getElementById("side-menu").style.display = "block";
+      }
+      document.getElementById("service").style.display = "block";
+      document.getElementById("admin-categories").style.display = "none";
+      break;
+    case "Service":
+      document.querySelector(".login-area").style.display = "none";
+      // this checks if browser is mobile
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        // some code..
+        document.getElementById("side-menu").style.display = "none";
+      } else {
+        document.getElementById("side-menu").style.display = "block";
+      }
+      document.getElementById("service").style.display = "none";
+      document.getElementById("admin-categories").style.display = "block";
+      break;
+    case "Update Service":
+      document.querySelector(".login-area").style.display = "none";
+      // this checks if browser is mobile
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        // some code..
+        document.getElementById("side-menu").style.display = "none";
+      } else {
+        document.getElementById("side-menu").style.display = "block";
+      }
+      document.getElementById("service").style.display = "block";
+      document.getElementById("admin-categories").style.display = "none";
+      document.getElementById("add-serviceTitle").innerHTML = "Update Serivice";
+      break;
+    case "Log Out":
+      document.querySelector(".login-area").style.display = "block";
+      document.getElementById("service").style.display = "none";
+      document.getElementById("admin-categories").style.display = "none";
+      document.getElementById("footer-admin").style.display = "none";
+      document.getElementById("side-menu").style.display = "none";
+      break;
+    default:
+      document.querySelector(".login-area").style.display = "block";
+      document.getElementById("footer-admin").style.display = "none";
+      document.getElementById("side-menu").style.display = "none";
+      break;
+  }
 };
 
-
-
-const login = document.querySelector('#login-btn');
-login.addEventListener('click', () => {
-    document.querySelector('.login-area').style.display="none";
-    document.getElementById('service').style.display = 'block';
-    document.getElementById('footer-admin').style.display= 'block';
+const login = document.querySelector("#login-btn");
+login.addEventListener("click", () => {
+  document.querySelector(".login-area").style.display = "none";
+  document.getElementById("service").style.display = "block";
+  document.getElementById("footer-admin").style.display = "block";
+  // this checks if browser is mobile
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // some code..
+    document.getElementById("side-menu").style.display = "none";
+  } else {
+    document.getElementById("side-menu").style.display = "block";
+  }
 });
 
-
-const submit = document.querySelector('#btn-submitService');
-submit.addEventListener('click', () => {
-    document.getElementById('service').style.display = 'none';
-    document.getElementById('admin-categories').style.display = 'block';
+const submit = document.querySelector("#btn-submitService");
+submit.addEventListener("click", () => {
+  document.getElementById("service").style.display = "none";
+  document.getElementById("admin-categories").style.display = "block";
 });
 
-
-for (var i = 0 ; i < Flist.length; i++) {
-    console.log(Flist[i]);
-    Flist[i].addEventListener('click', getUrl);
-  } 
+for (var i = 0; i < Flist.length; i++) {
+  console.log(Flist[i]);
+  Flist[i].addEventListener("click", getUrl);
+}
